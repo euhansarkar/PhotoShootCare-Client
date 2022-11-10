16e32d49442d550d759e588dd140aee653f81a55
euhansarkar <euhansarkar@gmail.com>
2022-11-10 16:53:21 +0600

    length problem solved

--- src/Pages/MyReview/SingleReview/SingleReview.js
+++ src/Pages/MyReview/SingleReview/SingleReview.js
@@ -17,7 +17,7 @@ const SingleReview = ({ review, handleDelete }) => {
             />
           </div>
           <div>
-            <h4 className="font-bold">{reviewer.length > 10 ? reviewer.slice(0, 10) : reviewer}</h4>
+            <h4 className="font-bold">{reviewer?.length > 10 ? reviewer.slice(0, 10) : reviewer}</h4>
             <span className="text-xs dark:text-gray-400">{address}</span>
           </div>
         </div>
@@ -29,7 +29,7 @@ const SingleReview = ({ review, handleDelete }) => {
           >
             <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
           </svg>
-          <span className="text-xl font-bold">{rating.length > 5 ? rating.slice(0, 5) : rating}</span>
+          <span className="text-xl font-bold">{rating?.length > 5 ? rating.slice(0, 5) : rating}</span>
         </div>
       </div>
       <div className="p-4 space-y-2 text-sm dark:text-gray-400">
--- src/Pages/ServiceDetails/AddReview/AddReview.js
+++ src/Pages/ServiceDetails/AddReview/AddReview.js
@@ -98,7 +98,7 @@ const AddReview = ({ serviceDetails }) => {
         </div>
         <div>
           <textarea
-           maxlength="120"
+           maxLength="120"
             className="textarea textarea-error w-full my-4"
             placeholder="Write Your Review Here"
             name="reviewText"
